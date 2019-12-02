49f3f4e07415cd05c9713ed1cea8e0a0e3913aca
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2019-12-02 17:22:25 -0500

    feat: complete tailwind install to read components.css and add Map component.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,13 +1,34 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const isProduction = EmberApp.env() === 'production';
+
+const purgeCSS = {
+  module: require('@fullhuman/postcss-purgecss'),
+  options: {
+    content: [
+      // add extra paths here for components/controllers which include tailwind classes
+      './app/index.html',
+      './app/components/**/*.hbs',
+      './app/templates/**/*.hbs'
+    ],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  }
+}
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     postcssOptions: {
       compile: {
         plugins: [
-          require("tailwindcss")
+          {
+            module: require('postcss-import'),
+            options: {
+              path: ['node_modules']
+            }
+          },
+          require('tailwindcss')('./app/tailwind/config.js'),
+          ...isProduction ? [purgeCSS] : []
         ]
       }
     }
